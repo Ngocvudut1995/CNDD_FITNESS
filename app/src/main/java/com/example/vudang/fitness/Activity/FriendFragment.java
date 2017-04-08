3d63d9b4fa75e8b6166d2fbaefd7f492c32d1220
Ngocvudut1995 <ngocvudut1995@gmail.com>
2017-04-08 08:44:55 +0700

    Hello
    dd

---
+++ app/src/main/java/com/example/vudang/fitness/Activity/FriendFragment.java
@@ -0,0 +1,52 @@
+package com.example.vudang.fitness.Activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vudang.fitness.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FriendFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FriendFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FriendFragment extends Fragment {
+    public FriendFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friend, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ app/src/main/java/com/example/vudang/fitness/Activity/HomeFragment.java
@@ -0,0 +1,88 @@
+package com.example.vudang.fitness.Activity;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.vudang.fitness.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Home.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Home#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+    private ImageButton btn_play;
+    private View containerView;
+    private FragmentDrawer.FragmentDrawerListener drawerListener;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    public void setDrawerListener(FragmentDrawer.FragmentDrawerListener listener) {
+        this.drawerListener = listener;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        btn_play = (ImageButton) rootView.findViewById(R.id.btn_play);
+        btn_play.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                FriendFragment fragment = null;
+                String title = getString(R.string.app_name);
+                        fragment = new FriendFragment();
+                        title = getString(R.string.title_friends);
+
+//
+
+                if (fragment != null) {
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.container_body, fragment);
+                    fragmentTransaction.commit();
+
+                    // set the toolbar title
+                    ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Excercise");
+                }
+            }
+        });
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+}
