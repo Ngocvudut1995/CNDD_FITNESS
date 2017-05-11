684dc0c4a6dcbf966bd74eba7ed5102d7c7a7ae9
Ngocvudut1995 <ngocvudut1995@gmail.com>
2017-05-11 14:37:21 +0700

    update new

--- app/src/main/java/com/example/vudang/fitness/DemoActivity.java
+++ app/src/main/java/com/example/vudang/fitness/DemoActivity.java
@@ -15,7 +15,6 @@ import android.widget.Chronometer;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-<<<<<<< HEAD
 import com.example.vudang.fitness.Model.DBHandler;
 import com.example.vudang.fitness.Model.Exersice;
 
@@ -52,7 +51,7 @@ public class DemoActivity extends AppCompatActivity {
         DBHandler db = new DBHandler(this);
         // Reading all shops
         //db.addExersice(new Exersice(0,"Full Body","1,2,3,4,5"));
-      //  db.addExersice(new Exersice(0,"Stretch Easy","1,2,3,4,5"));
+        //  db.addExersice(new Exersice(0,"Stretch Easy","1,2,3,4,5"));
         Log.d("Reading: ", "Reading all shops..");
         List<Exersice> list = db.getAllExersice();
 
@@ -61,37 +60,5 @@ public class DemoActivity extends AppCompatActivity {
 // Writing shops to log
             Log.d("Exersice: : ", log);
         }
-=======
-import com.example.vudang.fitness.Apdater.ItemExerciseAdapter;
-import com.example.vudang.fitness.Model.ItemExercise;
-
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
-
-public class DemoActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-   @Override
-    protected void onCreate(Bundle savedInstanceState){
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.framgment_select);
-       initView();
-   }
-    public void initView(){
-        recyclerView=(RecyclerView) findViewById(R.id.select_view);
-        recyclerView.setHasFixedSize(true);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
-        recyclerView.setLayoutManager(layoutManager);
-        ArrayList<ItemExercise> arrayList=new ArrayList<>();
-        arrayList.add(new ItemExercise(R.drawable.output_zczmrt,"Russian Twist","Một bài tập tuyệt vời cho cơ bụng. Nó cũng thúc đẩy các cơ bắp khác hoạt động. Nếu bạn vẫn còn là một người mới bắt đầu hoặc không có sức khoẻ và độ bền cơ bắp bài tập này sẽ giúp bạn thúc đẩy sức khỏe, kích thích việc ăn uống giúp bạn tăng cân và xây dựng cơ bắp như ý muốn."));
-        arrayList.add(new ItemExercise(R.drawable.sideplank_only,"Side Plank","Bài tập này là vũ khí bí mật giúp thắt chặt vòng eo của bạn. Nó hoạt động vào các cơ bắp nằm sâu dưới bụng."));
-        arrayList.add(new ItemExercise(R.drawable.output,"Medicine Ball Slam","Đây là một bài tập đơn giản và dễ dàng thực hiện"));
-        arrayList.add(new ItemExercise(R.drawable.output_3rd,"Medicine Ball V-Up","Đây là một bài tập thể dục tăng cường nhắm vào toàn bộ khu vực cốt lõi, nhấn mạnh vào các cơ bụng dưới và hông."));
-        arrayList.add(new ItemExercise(R.drawable.output_4,"Medicine Ball Toe Touch","Đây là động tác dễ dàng đối với hầu hết phụ nữ. Nếu bạn muốn nhìn thấy kết quả tốt hơn và một bài tập tác động đến toàn bộ cơ thể đây là động tác phù hợp. "));
-        arrayList.add(new ItemExercise(R.drawable.output_8,"Puncher's Push Up","Giữ cơ bụng của bạn để ổn định cơ thể. Đây là biến thể của động tác chống đẩy giúp thúc đẩy những cơ bắp cốt lõi."));
-        ItemExerciseAdapter itemExerciseAdapter=new ItemExerciseAdapter(arrayList,getApplicationContext());
-        recyclerView.setAdapter(itemExerciseAdapter);
->>>>>>> origin/master
     }
-
-
 }
